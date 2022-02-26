76765b94ec01d00a1992ae3ec00d4ec6b04f61bf
JonathonYY <43181115+JonathonYY@users.noreply.github.com>
2022-02-26 14:13:48 -0800

    Animations

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ function myFunction(){
 function playAudio() {
     const audio = document.getElementById("audio");
     audio.play();
-    if (audio.loop == false)
+    c = document.getElementById("container");
+    b = document.getElementById("button");
+    if (audio.loop == false){
         audio.loop = true;
+        c.style.animationDuration = "6s";
+        b.style.animationDuration = "6s";
+    }
     else {
         audio.loop = false;
         audio.pause();
         audio.currentTime = 0;
+        c.style.animationDuration = "0s";
+        b.style.animationDuration = "0s";
     }
 }
