39227b43d04c0b2ccc635b049bf8a00fbaf5b994
eliu0120 <48814349+eliu0120@users.noreply.github.com>
2022-02-26 12:59:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function myFunction(){
     clicks+=1;
     document.getElementById("clicks").innerHTML = clicks;
 }
+
+function playAudio() {
+    let audio = document.getElementById("audio");
+    audio.play();
+}
